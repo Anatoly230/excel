0a9854d9e0a8a41d0e05ed8d7b7c93afdf74b043
Anatoly230 <maximos225@mail.ru>
2021-12-25 03:53:07 +0300

    last correct before start

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-import "./module"
-console.log("working")
-=======
 import './module'
 
 console.log('Working!')
->>>>>>> 7e71befac77da44feb994921373c861263f88e56
--- src/module.js
+++ src/module.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-console.log("Module.js")
-=======
 console.log('Module.js')
->>>>>>> 7e71befac77da44feb994921373c861263f88e56
